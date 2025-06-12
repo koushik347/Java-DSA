652199b1495ff4153edf333acb4b7811a4e3f677
Koushik Reddy <koushikconsiderable06@gmail.com>
2025-06-12 19:03:32 +0530

    Add files via upload

---
+++ subarray equals K.py
@@ -0,0 +1,15 @@
+def countSubarrayequalsK(nums, K):
+    count = 0
+    n = len(nums)
+
+    for i in range(n):
+        sum_ = 0
+        for j in range(i, n):
+            sum_ += nums[j]
+            if sum_ == K:
+                count += 1
+    return count
+if __name__=="__main__":
+    nums = [1,2,3,4]
+    K=5
+    print(countSubarrayequalsK(nums, K))
